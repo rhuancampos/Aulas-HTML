97b015ee8cf6d175b810ea253148fea2e28c0681
Rhuan Campos <gleysonrhuan@gmail.com>
2026-04-15 21:38:56 -0300

    Atualização

--- uso-js/script.js
+++ uso-js/script.js
@@ -1,21 +1,31 @@
-function mostrarMensagem() {
+console.log("Teste");
+document.getElementById("titulo").innerText = "Teste atualizado";
+document.getElementsByClassName("titulo")[3].innerHTML = "Teste atualizado 2";
+document.querySelectorAll("h3.titulo")[0].innerText = "Teste atualizado 3";
+
+function mostrarTexto() {
   const nome = document.getElementById("nome").value;
   const idade = document.getElementById("idade").value;
-  console.debug(`Idade ${idade}`);
-  let mensagem = `Olá, ${nome}. Seja bem-vindo ao meu site! `;
-
-  if (idade < 0) {
-    alert("Idade inválida!");
-    return;
-  } else if (idade < 18) {
-    mensagem = mensagem + " Você é menor de idade.";
+  if (idade >= 18) {
+    console.log("Você é maior de idade");
   } else {
-    mensagem += "Você é maior de idade.";
+    console.log("Você é menor de idade");
   }
+  console.log(`O nome é: ${nome}`);
+}
 
-  alert(mensagem);
+function criarElemento() {
+  const novo = document.createElement("p");
+  const nome = document.getElementById("nome").value;
+  novo.innerText = nome;
+  document.body.appendChild(novo);
+
+  const novo2 = document.createElement("img");
+  novo2.src = "https://picsum.photos/300/90";
+  document.body.appendChild(novo2);
 }
 
 function mudarCor() {
-  document.body.style.backgroundColor = "lightblue";
+  /* document.body.style.backgroundColor = "#c0c0c0" */
+  document.body.style = "filter: invert(1) hue-rotate(180deg) !important; background-color: #000 !important;";
 }
